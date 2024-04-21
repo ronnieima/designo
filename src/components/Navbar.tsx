3cd94981325610a727864ba64b7a75bf2e4a1851
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-04-21 17:08:13 +0200

    work on mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,56 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import NavLinks from "./ui/NavLinks";
 import MaxWidthContainer from "./MaxWidthContainer";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { createPortal } from "react-dom";
 
 export default function Navbar() {
+  const [isOpenMobileNav, setIsOpenMobileNav] = useState(false);
+  console.log(isOpenMobileNav);
   return (
-    <nav className=" h-[96px] flex items-center">
-      <MaxWidthContainer className="flex-row justify-between w-full px-4 sm:px-0">
+    <>
+      <nav className=" flex h-[96px] items-center">
+        <MaxWidthContainer className="w-full flex-row justify-between px-4 sm:px-0">
+          <Link href={"/"}>
+            <Image
+              src={"/assets/shared/desktop/logo-dark.png"}
+              width={0}
+              height={0}
+              className="h-auto w-52"
+              sizes="100vw"
+              alt="Designo Logo"
+            />
+          </Link>
+          <NavLinks className="hidden sm:flex sm:flex-row sm:text-black" />
 
-      <Image
-        src={"/assets/shared/desktop/logo-dark.png"}
-        width={0}
-        height={0}
-        className="h-auto w-52"
-        sizes="100vw"
-        alt="Designo Logo"
-        />
-      <NavLinks className="hidden sm:flex sm:text-black sm:flex-row"/>
-      <Image
-        src={"/assets/shared/mobile/icon-hamburger.svg"}
-        width={0}
-        height={0}
-        className="size-6 sm:hidden"
-        sizes="25vw"
-        alt="Designo Logo"
-        />
+          <Button
+            className="sm:hidden"
+            variant={"ghost"}
+            onClick={() => setIsOpenMobileNav(!isOpenMobileNav)}
+          >
+            <Image
+              src={"/assets/shared/mobile/icon-hamburger.svg"}
+              width={0}
+              height={0}
+              className="size-6 "
+              sizes="25vw"
+              alt="Hamburger Menu"
+            />
+          </Button>
         </MaxWidthContainer>
-    </nav>
+      </nav>
+      {isOpenMobileNav &&
+        createPortal(
+          <div className="absolute top-[96px] h-full w-full bg-black/50">
+            <div className="  h-[235px]  bg-black text-white">
+              <ul></ul>
+            </div>
+          </div>,
+          document.body,
+        )}
+    </>
   );
 }
