c3b0e098f5e6f1df5b3c9f1ddf12793d1e173f5d
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-06-02 21:55:05 +0200

    finish locations page

--- src/app/locations/page.tsx
+++ src/app/locations/page.tsx
@@ -1,19 +1,26 @@
 "use client";
-import "leaflet/dist/leaflet.css";
-import SectionTemplate from "@/components/SectionTemplate";
-import { MapContainer, TileLayer } from "react-leaflet";
 import { locations } from "@/lib/content";
+import { cn } from "@/lib/utils";
 import { LatLngExpression } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer } from "react-leaflet";
 export default function LocationsPage() {
   return (
-    <main className="flex flex-col gap-10 py-8">
+    <main className="flex flex-col gap-10 py-8 md:gap-32 xl:gap-8 ">
       {locations.map((location, i) => (
-        <div key={location.value}>
-          <div>
+        <section
+          key={location.value}
+          className={cn(
+            `mx-auto flex w-full max-w-[689px] flex-col md:gap-6 xl:max-h-[326px] xl:max-w-[1111px] xl:flex-row`,
+            { "xl:flex-row-reverse": i !== 1 },
+          )}
+        >
+          <div className="">
             <MapContainer
               center={location.coords as LatLngExpression}
-              zoom={12}
-              className="h-64"
+              zoom={11}
+              className="h-80 md:rounded-xl xl:w-[350px]"
+              scrollWheelZoom={false}
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -21,9 +28,18 @@ export default function LocationsPage() {
               />
             </MapContainer>
           </div>
-          <div>
-            <div>{location.label}</div>
-            <div>
+          <div
+            className={
+              "flex w-full flex-col items-center justify-center gap-6 bg-lighterPeach py-20 text-center md:rounded-xl md:px-[74px] md:text-left"
+            }
+          >
+            <h2 className="text-peach md:self-start">{location.label}</h2>
+            <div
+              className={cn(
+                "flex w-full flex-col items-center gap-6",
+                "md:flex-row md:justify-start md:gap-32",
+              )}
+            >
               <div>
                 <p className="font-bold">{location.office}</p>
                 <p>{location.addressLineOne}</p>
@@ -36,7 +52,7 @@ export default function LocationsPage() {
               </div>
             </div>
           </div>
-        </div>
+        </section>
       ))}
     </main>
   );
