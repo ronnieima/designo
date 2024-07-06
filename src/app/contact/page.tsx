b3f6dc4a880c4303eb94cbd9d6f703207f7e8fe3
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-07-06 11:20:36 +0200

    implement shadcn and react-hook-form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,38 @@
+"use client";
+
 import React from "react";
 import LocationsSection from "../about/components/LocationsSection";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  name: z.string().min(1, "Required"),
+  email: z.string().email().min(1, "Required"),
+  phone: z.string().min(1, "Required"),
+  message: z.string().min(1, "Required"),
+});
 
 export default function ContactPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function handleSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <main className="flex flex-col items-center ">
       <section className="space-y-12 bg-peach px-6 py-20 text-white">
@@ -16,17 +45,35 @@ export default function ContactPage() {
             users, drop us a line.
           </p>
         </header>
-        <form className="flex flex-col gap-4">
-          <Input type="text" placeholder="Name" />
-          <Input type="email" placeholder="Email Address" />
-          <Input type="tel" placeholder="Phone" />
-          <Input
-            type="text"
-            placeholder="Your Message"
-            className="pb-32 pt-4"
-          />
-          <Button className="self-center px-8 py-4">SUBMIT</Button>
-        </form>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input {...field} type="text" placeholder="Name" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Input type="text" placeholder="Name" />
+            <Input type="email" placeholder="Email Address" />
+            <Input type="tel" placeholder="Phone" />
+            <Input
+              type="text"
+              placeholder="Your Message"
+              className="pb-32 pt-4"
+            />
+            <Button type="submit" className="self-center px-8 py-4">
+              SUBMIT
+            </Button>
+          </form>
+        </Form>
       </section>
       <LocationsSection />
     </main>
